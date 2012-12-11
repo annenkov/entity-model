906bc0ab42b7bea19753738ae03aafac0918da72
Danil Annenkov <support@ib-soft.ru>
2012-12-11 23:41:53 +0900

    Added string representation for entity

--- test/admin.py
+++ test/admin.py
@@ -1,18 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from sample.crm import models
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description',)
-
-
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('customer', 'name', 'phone', 'email',)
 
-
-
-
-admin.site.register(models.Customer, CustomerAdmin)
-admin.site.register(models.Contact, ContactAdmin)
 
--- test/models.py
+++ test/models.py
@@ -5,11 +5,19 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=256)         
     description = models.CharField(max_length=256)         
-           
+              
+    def __unicode__(self):
+        return "Customer: {0}".format(self.name)
+
+
 class Contact(models.Model):
     customer = models.ForeignKey(Customer)         
     name = models.CharField(max_length=256)         
     phone = models.CharField(max_length=256)         
     email = models.CharField(max_length=256)         
-           
+              
+    def __unicode__(self):
+        return "Contact: {0}".format(self.pk)
+
+
 
